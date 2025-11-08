1fdc8a9dfa15ddac095f994f82546c53a9e403d4
Cédric Belin <cedx@outlook.com>
2025-11-08 15:14:26 +0100

    Add the `New-Connection` class

---
+++ src/New-Connection.psm1
@@ -0,0 +1,36 @@
+using namespace System.Data
+
+<#
+.SYNOPSIS
+	Creates a new database connection.
+.PARAMETER Type
+	The type of connection class to instantiate.
+.PARAMETER ConnectionString
+	The connection string used to open the database.
+.PARAMETER Open
+	Value indicating whether to open the connection.
+.INPUTS
+	The connection string used to open the database.
+.OUTPUTS
+	The newly created connection.
+#>
+function New-Connection {
+	[CmdletBinding()]
+	[OutputType([IDbConnection])]
+	param (
+		[Parameter(Mandatory, Position = 0)]
+		[type] $Type,
+
+		[Parameter(Mandatory, Position = 1, ValueFromPipeline)]
+		[string] $ConnectionString,
+
+		[Parameter()]
+		[switch] $Open
+	)
+
+	process {
+		$connection = New-Object $Type.FullName $ConnectionString
+		if ($Open) { $connection.Open() }
+		$connection
+	}
+}
